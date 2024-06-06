c67096c8f469e8aa2bab17a973930a355edbc9f6
dinanr <153698587+dinanr@users.noreply.github.com>
2024-06-06 13:50:29 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="row p-3 bg-primary text-white">
         <div class="col-lg-1">
-            <img src="/assets/img/LOGO-SMK4.png" alt="logo SMK 4" height="100px" width="100px">
+            <img src="~/assets/img/LOGO-SMKN4 (4).png" alt="logo smkn4" height="100px" width="100px">
         </div>
-
         <div class="col-lg-11">
             <h1>Perpus Digital</h1>
             <address>
                 SMKN 4 Tasikmalaya
-                jl.Depok, Sukamenak, Purbaratu
+                Jl.Depok, Sukamenak, Purbaratu
             </address>
         </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -3,40 +3,126 @@
         <div class="row my-5">
             <div class="col-lg-6">
                 <nuxt-link to="/pengunjung/tambah">
-                <div class="card bg-pengunjung rounded-5">
-                    <div class="card-body">
-                        <h2>pengunjung</h2>
+                    <div class="card bg-pengunjung rounded-5">
+                        <div class="card-body">
+                            <h2>Pengunjung</h2>
                         </div>
                     </div>
                 </nuxt-link>
             </div>
-
             <div class="col-lg-6">
-                <nuxt-link to="/buku/">
-                <div class="card bg-buku rounded-5">
-                    <div class="card-body">
-                        <h2>Cari Buku</h2>
+                <nuxt-link to="/buku">
+                    <div class="card bg-buku rounded-5">
+                        <div class="card-body">
+                            <h2>Cari Buku</h2>
+                        </div>
                     </div>
-                </div>
                 </nuxt-link>
             </div>
         </div>
     </div>
+    <h2 style="margin: 30px;"><strong>STATISTIK</strong></h2>
+    <div class="container-fluid">
+        <div class="row justify-content-evenly rounded-3">
+            <div class="col-5">
+                <div class="card1">
+                    <div class="dina">
+                        <h2>{{ totalPengunjung }} Pengunjung</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5">
+                <div class="dina1">
+                    <h2>{{ totalBuku }} Buku</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="line">
+        <Statistik />
+    </div>
 </template>
 
+<script setup>
+const supabase = useSupabaseClient()
+
+const totalPengunjung = ref(0)
+
+async function getTotalPengunjung() {
+    const { count, error } = await supabase
+        .from('pengunjung')
+        .select('*', { count: 'exact', head: true })
+    if (error) throw error
+    if (count) totalPengunjung.value = count
+}
+const totalBuku = ref(0)
+
+async function getTotalBuku() {
+    const { count, error } = await supabase
+        .from('buku')
+        .select('*', { count: 'exact', head: true })
+    if (error) throw error
+    if (count) totalBuku.value = count
+}
+
+onMounted(() => {
+    getTotalPengunjung()
+    getTotalBuku()
+    
+    
+})
+</script>
+
 <style scoped>
+.v2 {
+    height: 3px;
+    width: 98%;
+    margin-left: 30px;
+    border-left: 3px solid black;
+    background-color: black;
+}
+
+.v1 {
+    margin-top: 40px;
+    margin-left: 30px;
+    border-left: 3px solid black;
+    height: 400px;
+}
+
 .card {
     height: 200px;
-    box-shadow: 1px 1px 10px #424242;
+    box-shadow: 1px 1px 5px #424242;
 }
+
 .card.bg-pengunjung {
-    background-image: url('../assets/img/bg-home-kunjungan.jpeg');
+    background-image: url('../assets/img/kunjungan.png');
     background-repeat: no-repeat;
     background-position: center center;
     background-size: cover;
 }
+
 .card.bg-buku {
-    background: url('../assets/img/buku.jpg');
+    background: url('../assets/img/cari buku.png') no-repeat center center;
     background-size: cover;
+}
+
+.dina {
+    height: 200px;
+    box-shadow: 1px 1px 10px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ECFF7A;
+}
+
+.dina1 {
+    height: 200px;
+    box-shadow: 1px 1px 10px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #99FF90;
 }
 </style>
